8075b494b8736c990a7f625236efc00bd4f85c50
Karolzp <zupnik.karol@gmail.com>
2019-01-17 16:46:00 +0100

    add game loop

--- src/com/Controller/GameLoop.java
+++ src/com/Controller/GameLoop.java
@@ -1,10 +1,36 @@
 package com.Controller;
 
+import javafx.application.Platform;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class GameLoop {
 
-    BoardController boardController;
+    private BoardController boardController;
+    private int speed = 1000;
+    private ScheduledExecutorService service;
+
 
     public GameLoop(BoardController boardController){
         this.boardController = boardController;
+        startLoop();
+    }
+
+    public void startLoop() {
+        service = Executors.newScheduledThreadPool(1);
+        service.scheduleAtFixedRate(oneStep(), 0, speed, TimeUnit.MILLISECONDS);
+    }
+
+    private Runnable oneStep() {
+        Runnable runnable = () -> {
+            try {
+                Platform.runLater(() -> boardController.newGeneration());
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        };
+        return runnable;
     }
 }
