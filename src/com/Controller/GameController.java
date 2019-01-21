3f593c65660cbc1874edb3f6aeac08897fa8f20b
Karolzp <zupnik.karol@gmail.com>
2019-01-21 12:42:35 +0100

    make start button working

--- src/com/Controller/GameController.java
+++ src/com/Controller/GameController.java
@@ -1,27 +1,40 @@
 package com.Controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.BorderPane;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
 import java.net.URL;
+import java.util.EventListener;
 import java.util.ResourceBundle;
 
 public class GameController implements Initializable {
 
-    public Button playBtn;
+    public Button startBtn;
     public Slider speedSlider;
     public BorderPane MainStage;
 
     public static BorderPane StaticMainStage;
 
+    private BoardController boardController;
+    private GameLoop gameLoop;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
         this.StaticMainStage = this.MainStage;
-        BoardController boardController = new BoardController();
-        GameLoop gameLoop = new GameLoop(boardController);
-
+        this.boardController = new BoardController();
+        this.gameLoop = new GameLoop(boardController);
     }
+
+    @FXML
+    void handleStartBtn(){
+        this.gameLoop.handleStartBtn(startBtn);
+    }
+
+
 }
--- src/com/Controller/GameLoop.java
+++ src/com/Controller/GameLoop.java
@@ -1,24 +1,48 @@
 package com.Controller;
 
 import javafx.application.Platform;
+import javafx.scene.control.Button;
 
+import java.awt.event.ActionEvent;
+import java.util.EventListener;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class GameLoop {
 
+    private  boolean isRunning = false;
     private BoardController boardController;
-    private int speed = 1000;
+    private int speed = 500;
     private ScheduledExecutorService service;
 
 
     public GameLoop(BoardController boardController){
         this.boardController = boardController;
-        startLoop();
     }
 
-    public void startLoop() {
+    public void handleStartBtn(Button startBtn){
+        if(isRunning){
+            stopGame();
+            startBtn.setText("START");
+        }else {
+            startGame();
+            startBtn.setText("STOP");
+        }
+    }
+
+    private void startGame(){
+        startLoop();
+        this.isRunning = true;
+
+    }
+
+    private void stopGame(){
+        service.shutdown();
+        this.isRunning = false;
+    }
+
+    private void startLoop() {
         service = Executors.newScheduledThreadPool(1);
         service.scheduleAtFixedRate(oneStep(), 0, speed, TimeUnit.MILLISECONDS);
     }
